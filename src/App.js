8cc1e6f9efb44f09309b765472ee5bc7561b9923
FrogParty <83928115+FrogParty@users.noreply.github.com>
2022-08-03 15:51:42 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Reset } from "styled-reset";
+import Template from "./components/Template";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Reset />
+      <Template />
+    </React.Fragment>
+  );
+};
+
+export default App;
